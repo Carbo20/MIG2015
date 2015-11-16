d2aea87304b5bf45198c5bd2062d13d4ac501295
Drazah <a.pendragon@hotmail.fr>
2015-11-16 23:48:13 +0100

    control character + vision

--- Assets/Scripts/Controllers/vision.cs
+++ Assets/Scripts/Controllers/vision.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class vision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Transform target;
+    public GameObject joueur;
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float radius = GetComponent<SphereCollider>().radius;
+        Vector3 centre = GetComponent<SphereCollider>().center;
+        Vector3 joueurc = joueur.transform.position;
+        transform.position = joueurc;
+        transform.LookAt(target);
+        transform.localPosition = Vector3.forward * 5;
+        
+
+
+    }
 }
