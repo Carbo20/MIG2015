e2683d78eddda7f3a58efd6a1099f35e2a369985
Vincent FOUQUET <v.fouquet@student.isartdigital.com>
2015-11-17 23:16:56 +0100

    Fog System

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,47 +12,108 @@ public class PlayerController : MonoBehaviour
 
 	private bool _playerRecovery = false;
 
-    public float invertx;
-    public float inverty;
-    public float vitesse;
-    private float ratiovitesse;
-    private Vector3 moveDirection = Vector3.zero;
-    private Vector3 lookDirection = Vector3.zero;
-    public GameObject vision;
+	public float invertx;
+	public float inverty;
+	public float vitesse;
+	private float ratiovitesse;
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 lookDirection = Vector3.zero;
+	public GameObject vision;
 
-    public void Update()
+	public void Update()
 	{
-        ratiovitesse = (float)(vitesse / 100);
+		ratiovitesse = (float) (vitesse/100);
 
-        if (_characterController)
+		if (_characterController)
 		{
-            moveDirection = new Vector3((invertx) * Input.GetAxis("Horizontal"), 0f,(inverty) * Input.GetAxis("Vertical"));
-            moveDirection *= ratiovitesse;
+			moveDirection = new Vector3((invertx)*Input.GetAxis("Horizontal"), 0f, (inverty)*Input.GetAxis("Vertical"));
+			moveDirection *= ratiovitesse;
 			_characterController.Move(moveDirection);
 
-            lookDirection = new Vector3(-1*Input.GetAxis("Horizontal2"), 0f, Input.GetAxis("Vertical2"));
-            // transform.LookAt(lookDirection);
-            if (lookDirection.x > 0 && lookDirection.z == 0) {transform.rotation= Quaternion.Euler(0f, 0f, 0f); }
-            if (lookDirection.x > 0 && lookDirection.z > 0) { transform.rotation = Quaternion.Euler(0f, 45, 0f); }
-            if (lookDirection.x == 0 && lookDirection.z > 0) { transform.rotation = Quaternion.Euler(0f, 90, 0f); }
-            if (lookDirection.x < 0 && lookDirection.z > 0) { transform.rotation = Quaternion.Euler(0f, 135, 0f); }
-            if (lookDirection.x < 0 && lookDirection.z == 0) { transform.rotation = Quaternion.Euler(0f, 180, 0f); }
-            if (lookDirection.x < 0 && lookDirection.z < 0) { transform.rotation = Quaternion.Euler(0f, 225, 0f); }
-            if (lookDirection.x == 0 && lookDirection.z < 0) { transform.rotation = Quaternion.Euler(0f, 270, 0f); }
-            if (lookDirection.x > 0 && lookDirection.z < 0) { transform.rotation = Quaternion.Euler(0f, 315, 0f); }
+			lookDirection = new Vector3(-1*Input.GetAxis("Horizontal2"), 0f, Input.GetAxis("Vertical2"));
+			// transform.LookAt(lookDirection);
+			if (lookDirection.x > 0 && lookDirection.z == 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+			}
+			if (lookDirection.x > 0 && lookDirection.z > 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 45, 0f);
+			}
+			if (lookDirection.x == 0 && lookDirection.z > 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 90, 0f);
+			}
+			if (lookDirection.x < 0 && lookDirection.z > 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 135, 0f);
+			}
+			if (lookDirection.x < 0 && lookDirection.z == 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 180, 0f);
+			}
+			if (lookDirection.x < 0 && lookDirection.z < 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 225, 0f);
+			}
+			if (lookDirection.x == 0 && lookDirection.z < 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 270, 0f);
+			}
+			if (lookDirection.x > 0 && lookDirection.z < 0)
+			{
+				transform.rotation = Quaternion.Euler(0f, 315, 0f);
+			}
 
 
 			if (_spriteTransform)
 			{
 				_spriteTransform.rotation = Quaternion.identity;
 			}
-        }
 
-        
+			ParticleSystem emitter = FindObjectOfType<ParticleSystem>();
+
+			ParticleSystem.Particle[] particles = new ParticleSystem.Particle[emitter.maxParticles];
+			emitter.GetComponent<ParticleSystem>().GetParticles(particles);
+
+			CapsuleCollider collider = GetComponent<CapsuleCollider>();
+
+			foreach (ParticleSystem.Particle particle in particles)
+			{
+				if (collider.bounds.Contains(particle.position))
+				{
+					AddDamage(1);
+					break;
+				}
+			}
+		}
+	}
+
+	public void OnBlow()
+	{
+		ParticleSystem emitter = FindObjectOfType<ParticleSystem>();
+
+		if (!emitter) return;
+
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[emitter.maxParticles];
+		emitter.GetComponent<ParticleSystem>().GetParticles(particles);
+
+		CapsuleCollider collider = GetComponent<CapsuleCollider>();
+
+		List<ParticleSystem.Particle> particleList = new List<ParticleSystem.Particle>();
+
+		foreach (ParticleSystem.Particle particle in particles)
+		{
+			if (!collider.bounds.Contains(particle.position))
+				particleList.Add(particle);
+		}
+
+		emitter.SetParticles(particleList.ToArray(), particleList.Count);
 	}
 
 	public void AddDamage(int damage)
 	{
+		Debug.Log(_playerRecovery);
 		if (_playerRecovery) return;
 
 		_playerLifePoint -= damage;
@@ -60,18 +122,18 @@ public class PlayerController : MonoBehaviour
 		{
 			//TODO
 			//GameOverFunc
+			GameMgr.Instance.GameOver();
 		}
 		else
 		{
+			_playerRecovery = true;
 			StartCoroutine("PlayerRecoveryCoroutine");
 		}
 	}
 
-	IEnumerable<WaitForSeconds> PlayerRecoveryCoroutine()
+	IEnumerator PlayerRecoveryCoroutine()
 	{
-		_playerRecovery = true;
-		yield return new WaitForSeconds(_playerRecoveryTime);
+		yield return new WaitForSeconds(2f);
 		_playerRecovery = false;
 	}
-
 }
