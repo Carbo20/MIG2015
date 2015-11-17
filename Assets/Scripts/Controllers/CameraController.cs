870e768c3fbf7bb8addbbe4b26ff9f603c229fb8
Vincent FOUQUET <v.fouquet@student.isartdigital.com>
2015-11-17 12:11:05 +0100

    Camera prefab

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -6,23 +6,25 @@ public class CameraController : MonoBehaviour
 {
 	#region Fields
 
-	[SerializeField] private GameObject _followTarget;
-	[SerializeField] private BoxCollider _cameraZone;
+	[SerializeField] private Transform _followTarget;
+	[SerializeField] private float _cameraHeight = 10f;
+	[SerializeField] private float _cameraDepth = 5f;
+	//[SerializeField] private BoxCollider _cameraZone;
 
 	private Camera _camera;
-	private List<MeshCollider> _planeColliderList = new List<MeshCollider>();
-	private List<Plane> _planeList;
+//	private List<MeshCollider> _planeColliderList = new List<MeshCollider>();
+//	private List<Plane> _planeList;
 
 	#endregion
 
 	public void Awake()
 	{
-		_camera = GetComponent<Camera>();
+//		_camera = GetComponent<Camera>();
 	}
 
 	public void Start()
 	{
-		Camera cam = Camera.main;
+		/*Camera cam = Camera.main;
 		_planeList = new List<Plane>(GeometryUtility.CalculateFrustumPlanes(cam));
 		int i = 0;
 
@@ -38,12 +40,15 @@ public class CameraController : MonoBehaviour
 			p.transform.SetParent(_camera.transform);
 			_planeColliderList.Add(p.GetComponent<MeshCollider>());
 			i++;
-		}
+		}*/
 	}
 
 	public void Update()
 	{
-		float xBound = _planeColliderList[1].transform.localPosition.x;
+
+		transform.position = new Vector3(_followTarget.position.x, _cameraHeight, _followTarget.position.z - _cameraDepth);
+		
+		/*float xBound = _planeColliderList[1].transform.localPosition.x;
 		float zBound = _planeColliderList[3].transform.localPosition.y;
 
 		_camera.transform.position = new Vector3(_followTarget.transform.position.x, _camera.transform.position.y, _followTarget.transform.position.z);
@@ -55,7 +60,6 @@ public class CameraController : MonoBehaviour
 		if (!_cameraZone.bounds.Intersects(_planeColliderList[2].bounds))
 			_camera.transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, -1f * _cameraZone.transform.localScale.z * .5f + zBound);
 		else if (!_cameraZone.bounds.Intersects(_planeColliderList[3].bounds))
-			_camera.transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, 1f * _cameraZone.transform.localScale.z * .5f - zBound);
-		
+			_camera.transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, 1f * _cameraZone.transform.localScale.z * .5f - zBound);*/
 	}
 }
