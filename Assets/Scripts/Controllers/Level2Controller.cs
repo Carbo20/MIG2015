869734d388a5181b38d6f8e546ca545e0113e8f7
carbo20 <carbo20@hotmail.fr>
2015-11-18 14:13:35 +0100

    win condition level 2

---
+++ Assets/Scripts/Controllers/Level2Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Controller : MonoBehaviour {
+
+	[SerializeField] private string _musicName;
+	
+	public void Start()
+	{
+		AudioMgr.Instance.PlayMusic (_musicName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerController player = FindObjectOfType<PlayerController> ();
+		
+		CapsuleCollider pCollider = player.GetComponent<CapsuleCollider> ();
+		BoxCollider collider = GameObject.Find("WinAreaCondition").GetComponent<BoxCollider>();
+		
+		if (collider.bounds.Intersects (pCollider.bounds)) 
+		{
+			GameMgr.Instance.LoadLevel(3);
+		}
+	}
+}
